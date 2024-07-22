91d5854d399957e086f9cc203219375ce47f08c2
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-07-22 09:55:16 +0900

    테스트 vs

---
+++ Test/Test.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+using namespace std;
+
+//
+//// 스트링 모음 문자열 지우기..
+//string solution(string my_string) {
+//    string answer = "";
+//    string del_str = "aeiou";
+//
+//    for (char a : my_string)
+//    {
+//        if (del_str.find(a) == string::npos)
+//        {
+//            answer += a;
+//        }
+//
+//    }
+//
+//
+//    return answer;
+//}
+
+
+//string solution(string rsp) {
+//    string answer = "";
+//
+//    for (char ch : rsp)
+//    {
+//        if (ch == '2')
+//            answer += '0';
+//        else if (ch == '0')
+//            answer += '5';
+//        else if (ch == '5')
+//            answer += '2';
+//    }
+//    
+//
+//    return answer;
+//}
+
+
+
+
+int main()
+{
+    
+    return 0;
+}
+
